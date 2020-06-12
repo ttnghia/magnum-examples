71bfa14524f13d23daddbc4a6099611791c8646e
Nghia Truong <nghiatruong.vn@gmail.com>
2020-06-12 15:23:55 -0600

    Cleanup

--- src/octree/OctreeExample.cpp
+++ src/octree/OctreeExample.cpp
@@ -29,31 +29,23 @@
  */
 
 #include <Corrade/Containers/Pointer.h>
-#include <Corrade/Utility/StlMath.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Renderer.h>
-#include <Magnum/GL/PixelFormat.h>
 #include <Magnum/GL/Context.h>
+#include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/Version.h>
 #include <Magnum/Math/Color.h>
-#include <Magnum/Math/FunctionsBatch.h>
 #include <Magnum/MeshTools/Compile.h>
 #include <Magnum/Platform/Sdl2Application.h>
-#include <Magnum/Primitives/Circle.h>
+#include <Magnum/Primitives/Icosphere.h>
 #include <Magnum/SceneGraph/Camera.h>
 #include <Magnum/SceneGraph/Drawable.h>
 #include <Magnum/SceneGraph/MatrixTransformation3D.h>
 #include <Magnum/SceneGraph/Scene.h>
-#include <Magnum/Timeline.h>
+#include <Magnum/Shaders/Phong.h>
 #include <Magnum/Trade/MeshData.h>
 
-#include <Magnum/Primitives/Icosphere.h>
-#include <Magnum/Shaders/Phong.h>
-
-#include "DrawableObjects/ParticleGroup2D.h"
-#include "DrawableObjects/WireframeObject2D.h"
-#include "FluidSolver/ApicSolver2D.h"
-
+#include "../arcball/ArcBallCamera.h"
 #include "../motionblur/Icosphere.h"
 
 namespace Magnum { namespace Examples {
@@ -65,19 +57,24 @@ protected:
     void viewportEvent(ViewportEvent& event) override;
     void keyPressEvent(KeyEvent& event) override;
     void drawEvent() override;
+    void mousePressEvent(MouseEvent& event) override;
+    void mouseReleaseEvent(MouseEvent& event) override;
+    void mouseMoveEvent(MouseMoveEvent& event) override;
+    void mouseScrollEvent(MouseScrollEvent& event) override;
 
     /* Scene and drawable group must be constructed before camera and other
        drawble objects */
-    Containers::Pointer<Scene3D>                     scene;
-    Containers::Pointer<SceneGraph::DrawableGroup3D> drawables;
+    Containers::Pointer<Scene3D>                     _scene;
+    Containers::Pointer<SceneGraph::DrawableGroup3D> _drawables;
 
     /* Camera helpers */
     Containers::Pointer<Object3D>             _objCamera3D;
     Containers::Pointer<SceneGraph::Camera3D> _camera3D;
+    Containers::Pointer<ArcBallCamera>        _arcballCamera;
 
     Object3D*                           spheres[3];
-    Containers::Pointer<GL::Mesh>       mesh;
-    Containers::Pointer<Shaders::Phong> shader;
+    Containers::Pointer<GL::Mesh>       _mesh;
+    Containers::Pointer<Shaders::Phong> _shader;
 
     bool _pausedMotion = false;
 };
@@ -102,51 +99,58 @@ OctreeExample::OctreeExample(const Arguments& arguments) : Platform::Application
     /* Setup scene objects and camera */
     {
         /* Setup scene objects */
-        scene.emplace();
-        drawables.emplace();
+        _scene.emplace();
+        _drawables.emplace();
 
         /* Configure camera */
-        _objCamera3D.emplace(scene.get());
+        _objCamera3D.emplace(_scene.get());
         _objCamera3D->translate(Vector3::zAxis(3.0f));
 
         _camera3D.emplace(*_objCamera3D);
         _camera3D->setAspectRatioPolicy(SceneGraph::AspectRatioPolicy::Extend)
             .setProjectionMatrix(Matrix4::perspectiveProjection(35.0_degf, 4.0f / 3.0f, 0.001f, 1000.0f))
             .setViewport(GL::defaultFramebuffer.viewport().size());
+
+        const Vector3 eye{ Vector3::zAxis(5.0f) };
+        const Vector3 viewCenter{ };
+        const Vector3 up{ Vector3::yAxis() };
+        const Deg     fov = 45.0_degf;
+        _arcballCamera.emplace(*_scene.get(), eye, viewCenter, up, fov, windowSize(), framebufferSize());
+        _arcballCamera->setLagging(0.85f);
     }
 
-    mesh.emplace();
-    *mesh = MeshTools::compile(Primitives::icosphereSolid(3));
-    shader.emplace();
+    _mesh.emplace();
+    *_mesh = MeshTools::compile(Primitives::icosphereSolid(3));
+    _shader.emplace();
 
     /* Setup point (render as spheres) */ {
-        spheres[0] = new Object3D(scene.get());
-        (new Icosphere(mesh.get(), shader.get(), 0xff0000_rgbf, spheres[0], drawables.get()))
+        spheres[0] = new Object3D(_scene.get());
+        (new Icosphere(_mesh.get(), _shader.get(), 0xff0000_rgbf, spheres[0], _drawables.get()))
             ->translate(Vector3::yAxis(0.25f));
-        (new Icosphere(mesh.get(), shader.get(), 0xff0000_rgbf, spheres[0], drawables.get()))
+        (new Icosphere(_mesh.get(), _shader.get(), 0xff0000_rgbf, spheres[0], _drawables.get()))
             ->translate(Vector3::yAxis(0.25f))
             .rotateZ(120.0_degf);
-        (new Icosphere(mesh.get(), shader.get(), 0xff0000_rgbf, spheres[0], drawables.get()))
+        (new Icosphere(_mesh.get(), _shader.get(), 0xff0000_rgbf, spheres[0], _drawables.get()))
             ->translate(Vector3::yAxis(0.25f))
             .rotateZ(240.0_degf);
 
-        spheres[1] = new Object3D(scene.get());
-        (new Icosphere(mesh.get(), shader.get(), 0x00ff00_rgbf, spheres[1], drawables.get()))
+        spheres[1] = new Object3D(_scene.get());
+        (new Icosphere(_mesh.get(), _shader.get(), 0x00ff00_rgbf, spheres[1], _drawables.get()))
             ->translate(Vector3::yAxis(0.50f));
-        (new Icosphere(mesh.get(), shader.get(), 0x00ff00_rgbf, spheres[1], drawables.get()))
+        (new Icosphere(_mesh.get(), _shader.get(), 0x00ff00_rgbf, spheres[1], _drawables.get()))
             ->translate(Vector3::yAxis(0.50f))
             .rotateZ(120.0_degf);
-        (new Icosphere(mesh.get(), shader.get(), 0x00ff00_rgbf, spheres[1], drawables.get()))
+        (new Icosphere(_mesh.get(), _shader.get(), 0x00ff00_rgbf, spheres[1], _drawables.get()))
             ->translate(Vector3::yAxis(0.50f))
             .rotateZ(240.0_degf);
 
-        spheres[2] = new Object3D(scene.get());
-        (new Icosphere(mesh.get(), shader.get(), 0x0000ff_rgbf, spheres[2], drawables.get()))
+        spheres[2] = new Object3D(_scene.get());
+        (new Icosphere(_mesh.get(), _shader.get(), 0x0000ff_rgbf, spheres[2], _drawables.get()))
             ->translate(Vector3::yAxis(0.75f));
-        (new Icosphere(mesh.get(), shader.get(), 0x0000ff_rgbf, spheres[2], drawables.get()))
+        (new Icosphere(_mesh.get(), _shader.get(), 0x0000ff_rgbf, spheres[2], _drawables.get()))
             ->translate(Vector3::yAxis(0.75f))
             .rotateZ(120.0_degf);
-        (new Icosphere(mesh.get(), shader.get(), 0x0000ff_rgbf, spheres[2], drawables.get()))
+        (new Icosphere(_mesh.get(), _shader.get(), 0x0000ff_rgbf, spheres[2], _drawables.get()))
             ->translate(Vector3::yAxis(0.75f))
             .rotateZ(240.0_degf);
     }
@@ -165,7 +169,10 @@ void OctreeExample::drawEvent() {
     if(!_pausedMotion) {
         //
     }
-    _camera3D->draw(*drawables);
+
+    _arcballCamera->update();
+    //    _camera3D->draw(*drawables);
+    _arcballCamera->draw(*_drawables);
 
     swapBuffers();
     /* Run next frame immediately */
@@ -175,6 +182,7 @@ void OctreeExample::drawEvent() {
 void OctreeExample::viewportEvent(ViewportEvent& event) {
     /* Resize the main framebuffer */
     GL::defaultFramebuffer.setViewport({ {}, event.framebufferSize() });
+    _arcballCamera->reshape(event.windowSize(), event.framebufferSize());
 
     /* Recompute the camera's projection matrix */
     _camera3D->setViewport(event.framebufferSize());
@@ -191,6 +199,44 @@ void OctreeExample::keyPressEvent(KeyEvent& event) {
             break;
         default:;
     }
+}
+
+void OctreeExample::mousePressEvent(MouseEvent& event) {
+    /* Enable mouse capture so the mouse can drag outside of the window */
+    /** @todo replace once https://github.com/mosra/magnum/pull/419 is in */
+    SDL_CaptureMouse(SDL_TRUE);
+
+    _arcballCamera->initTransformation(event.position());
+
+    event.setAccepted();
+    redraw(); /* camera has changed, redraw! */
+}
+
+void OctreeExample::mouseReleaseEvent(MouseEvent&) {
+    /* Disable mouse capture again */
+    /** @todo replace once https://github.com/mosra/magnum/pull/419 is in */
+    SDL_CaptureMouse(SDL_FALSE);
+}
+
+void OctreeExample::mouseMoveEvent(MouseMoveEvent& event) {
+    if(!event.buttons()) { return; }
+
+    if(event.modifiers() & MouseMoveEvent::Modifier::Shift) {
+        _arcballCamera->translate(event.position());
+    } else { _arcballCamera->rotate(event.position()); }
+
+    event.setAccepted();
+    redraw(); /* camera has changed, redraw! */
+}
+
+void OctreeExample::mouseScrollEvent(MouseScrollEvent& event) {
+    const Float delta = event.offset().y();
+    if(Math::abs(delta) < 1.0e-2f) { return; }
+
+    _arcballCamera->zoom(delta);
+
+    event.setAccepted();
+    redraw(); /* camera has changed, redraw! */
 }
 } }
 
